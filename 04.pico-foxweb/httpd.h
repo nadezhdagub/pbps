8032da532d62189170d2c434b036a2d8e203f7d7
Nadezhda Gubenko <41242817+nadezhdagub@users.noreply.github.com>
2022-06-07 23:07:15 +0300

    Update httpd.h

--- 04.pico-foxweb/httpd.h
+++ 04.pico-foxweb/httpd.h
@@ -4,16 +4,18 @@
 #include <stdio.h>
 #include <string.h>
 
-// Client request
+// Запрос клиента
 extern char *method, // "GET" or "POST"
     *uri,            // "/index.html" things before '?'
     *qs,             // "a=1&b=2" things after  '?'
     *prot,           // "HTTP/1.1"
-    *payload;        // for POST
+    *payload,        // for POST
+    *message_log;
 
 extern int payload_size;
 
-// Server control functions
+
+// Функции управления сервером
 void serve_forever(const char *PORT);
 
 char *request_header(const char *name);
@@ -24,19 +26,18 @@ typedef struct {
 static header_t reqhdr[17] = {{"\0", "\0"}};
 header_t *request_headers(void);
 
-// user shall implement this function
+// user shall implement this function пользователь должен реализовать эту функцию
 
 void route();
-
-// Response
+// Response Ответ
 #define RESPONSE_PROTOCOL "HTTP/1.1"
 
-#define HTTP_200 printf("%s 200 OK\n\n", RESPONSE_PROTOCOL)
-#define HTTP_201 printf("%s 201 Created\n\n", RESPONSE_PROTOCOL)
-#define HTTP_404 printf("%s 404 Not found\n\n", RESPONSE_PROTOCOL)
-#define HTTP_500 printf("%s 500 Internal Server Error\n\n", RESPONSE_PROTOCOL)
+#define HTTP_200 printf("%s 200 OK\n\n", RESPONSE_PROTOCOL); sprintf(message_log, "%s 200", message_log)
+#define HTTP_201 printf("%s 201 Created\n\n", RESPONSE_PROTOCOL); sprintf(message_log, "%s 201", message_log)
+#define HTTP_404 printf("%s 404 Not found\n\n", RESPONSE_PROTOCOL); sprintf(message_log, "%s 404", message_log)
+#define HTTP_500 printf("%s 500 Internal Server Error\n\n", RESPONSE_PROTOCOL); sprintf(message_log, "%s 500", message_log)
 
-// some interesting macro for `route()`
+// some interesting macro for `route()` какой-нибудь интересный макрос для
 #define ROUTE_START() if (0) {
 #define ROUTE(METHOD, URI)                                                     \
   }                                                                            \
